e3198ac8cd4cab0c7ad718a4603dc0924423a1f9
Edson's Pi <edson@smith.net>
2017-04-02 09:15:24 +0000

    cleint Code changes to class based system

--- client.py
+++ client.py
@@ -1,20 +1,33 @@
 import socket
 import sys
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('192.168.0.2', 10000)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
 
-while True:
-        try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.connect(server_address)
-            message=raw_input('Message: ')
-            if message=='quit':
-                break
-            sock.sendall(message)
-        except:
-            break
-sock.close()
+class cInstance():
+    def __init__(self):
+        self.sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_address = ("192.168.0.2", 10000)
+        self.sock.connect(self.server_address)
 
+
+    def sendMessage(self,message):
+        sock=self.sock
+        sock.send(message)
+
+    def recieveFile(self,fileName):
+        sock=self.sock
+        with open(fileName,"wb") as f:
+            while True:
+                data=sock.recv(1024)
+                if not data:
+                    break
+                f.write(data)
+            f.close()
+            print ("successfull file transfer")
+    def close(self):
+        self.sock.close()
+
+
+c=cInstance()
+c.sendMessage("this is a test of the emergency broadcast system")
+c.recieveFile("test.txt")
+c.close()
